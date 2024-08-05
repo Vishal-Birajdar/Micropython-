a6a342aa90e030de9026871f81a2cdaacf0c45b3
Vishal-Birajdar <151386892+Vishal-Birajdar@users.noreply.github.com>
2024-08-05 17:08:38 +0530

    Update esecurity.py

--- esecurity.py
+++ esecurity.py
@@ -5,20 +5,6 @@ import shared
 import machine
 import json
 
-pixPin = 22
-Pixsize = 5
-pix = neopixel.NeoPixel(Pin(pixPin),Pixsize)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-WHITE = (255, 255, 255)
-ORANGE = (255,165,0)
-
-
 class call_loop:
     def __init__(self):
         pass
