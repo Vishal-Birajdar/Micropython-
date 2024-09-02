ffa5ba0fb12b4cbc8585e35be7253e5896a99127
Vishal-Birajdar <151386892+Vishal-Birajdar@users.noreply.github.com>
2024-09-02 11:23:19 +0530

    Update shared.py

--- shared.py
+++ shared.py
@@ -1,18 +1,20 @@
-from machine import Pin
+#from machine import Pin
 import json
-import ota
 import neopixel
+import time
+import machine
+rtc = machine.RTC()
+t = rtc.datetime()
 
 client = None
-wlan = 0
 arm_status = True
 FG_Arm = False
 act_sensor_cnt = 0
 
-R1State = Pin(2,Pin.OUT)
-R2State = Pin(3,Pin.OUT)
-R3State = Pin(4,Pin.OUT)
-R4State = Pin(5,Pin.OUT)
+R1State = machine.Pin(2,machine.Pin.OUT)
+R2State = machine.Pin(3,machine.Pin.OUT)
+R3State = machine.Pin(4,machine.Pin.OUT)
+R4State = machine.Pin(5,machine.Pin.OUT)
 active_sensor=[0,0,0,0]
 
 f = open('config.json',"r")
@@ -28,7 +30,7 @@ topic_hb = config['device_info']['c_code']+"/"+config['device_info']['a_code']+"
 
 pixPin = 22
 Pixsize = 5
-pix = neopixel.NeoPixel(Pin(pixPin),Pixsize)
+pix = neopixel.NeoPixel(machine.Pin(pixPin),Pixsize)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 150, 0)
@@ -38,4 +40,33 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 WHITE = (255, 255, 255)
 ORANGE = (255,165,0)
+#------------------------------------------__________________------------------------------------------
+
+file_name = ""
+
+def update_filename():
+    global file_name
+    # Get the current local time
+    t = time.localtime()
+    # Create a filename based on the current date
+    file_name = '{:04d}_{:02d}_{:02d}.txt'.format(t[0], t[1], t[2])
+
+#file_name = '{:04d}_{:02d}_{:02d}_{:02d}_{:02d}_{:02d}.txt'.format(t[0], t[1], t[2], t[4], t[5], t[6])
+
+def event(msg):
+    if file_name == "" or file_name != '{:04d}_{:02d}_{:02d}.txt'.format(time.localtime()[0], time.localtime()[1], time.localtime()[2]):
+        update_filename()
+    ts = time.localtime()
+    ts = time.localtime()
+    #ts_str = '{:04d}_{:02d}_{:02d}.txt'.format(time.localtime()[0], time.localtime()[1], time.localtime()[2])
+    #ts_str = "[%04d-%02d-%02d-%02d:%02d:%02d]" % (ts.tm_year, ts.tm_mon, ts.tm_mday, ts.tm_hour, ts.tm_min, ts.tm_sec)
+    #ts_str = "[%04d-%02d-%02d %02d:%02d:%02d]" % (ts.tm_year, ts.tm_mon, ts.tm_mday, ts.tm_hour, ts.tm_min, ts.tm_sec)
+    log_entry = f"{ts} {msg}\n"
+    
+    with open(file_name, 'a') as file:
+        file.write(log_entry)
+        
+    return log_entry       
+   
+#-------------------------------------------------________________________--------------------------------------------------- 
 
